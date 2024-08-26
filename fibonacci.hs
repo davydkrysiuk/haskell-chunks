2089205be020b4c2697c3604d58f6ca0b2640b40
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-08-26 06:06:54 +0100

    Update fibonacci.hs
    I might have broken it, but do I care?

--- fibonacci.hs
+++ fibonacci.hs
@@ -3,8 +3,5 @@ fibon 0 = 0
 fibon 1 = 1
 fibon n = fibon (n-1) + fibon(n-2)
 
-genFibo :: Int -> [Int]
-genFibo n = map (\x -> fibonacci x) [1..n]
-
 main :: IO()
-main = show $ genFibo 14
+main = show $ fibon 5
